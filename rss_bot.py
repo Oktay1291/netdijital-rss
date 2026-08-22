685f82289f735eae4d511a2134027f5cc8ad2b2c
Oktay1291 <ktysarikaya@gmail.com>
2026-08-22 17:21:20 +0300

    Update rss_bot.py

--- rss_bot.py
+++ rss_bot.py
@@ -12,8 +12,7 @@ BLOGGER_EMAIL = "ktysarikaya.netdijital1291@blogger.com"
 
 genai.configure(api_key=GEMINI_API_KEY)
 # En kararlı çalışan güncel model adı
-model = genai.GenerativeModel('gemini-2.5-flash')
-
+model = genai.GenerativeModel('gemini-3.6-flash')
 RSS_FEEDS = [
     "https://techcrunch.com/feed/" # Test için şimdilik ilk beslemeyle başlayalım
 ]
